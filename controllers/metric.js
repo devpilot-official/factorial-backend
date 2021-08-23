ac62dc6b786cd4742871e6ddf3c6752e34d93f42
Muhammed Saifudeen Salaudeen <mr.salaudeen.official@gmail.com>
2021-08-23 03:59:59 +0100

    chore: returning all metrics

--- controllers/metric.js
+++ controllers/metric.js
@@ -1,7 +1,8 @@
+const date = require("date.js");
 const Metric = require('mongoose').model('Metric');
 
 exports.getMetrics = async (req, res) => {
-    await Metric.find({}, "-updatedAt -__v")
+    await Metric.find({}, "-updatedAt -__v").sort({ createdAt: "descending" })
     .then((metrics) => {
         res.send({
             status: "success",
@@ -15,6 +16,54 @@ exports.getMetrics = async (req, res) => {
             data: null
         });  
     })
+}
+
+exports.filterMetrics = async (req, res) => {
+    const filter = req.query.filter;
+    let queryDate, metrics;
+
+    try {
+        switch (filter) {
+            case "minute":
+                queryDate = date("1 minute ago");
+                metrics = await Metric.find({ $and: [
+                    { createdAt: { $gte: queryDate } },
+                    { createdAt: { $lte: new Date() } }
+                ]}, "-updatedAt -__v").sort({ createdAt: "descending" }).exec();
+                break;
+            case "hour":
+                queryDate = date("1 hour ago");
+                metrics = await Metric.find({ $and: [
+                    { createdAt: { $gte: queryDate } },
+                    { createdAt: { $lte: new Date() } }
+                ]}, "-updatedAt -__v").sort({ createdAt: "descending" }).exec();
+                break; 
+            case "day":
+                queryDate = date("1 day ago");
+                metrics = await Metric.find({ $and: [
+                    { createdAt: { $gte: queryDate } },
+                    { createdAt: { $lte: new Date() } }
+                ]}, "-updatedAt -__v").sort({ createdAt: "descending" }).exec();
+                break;
+            
+            default:
+                metrics = await Metric.find({}).sort({ createdAt: "descending" });
+                break;
+        }
+        
+        res.send({
+            status: "success",
+            message: `last ${filter} metrics`,
+            data: metrics
+        });
+    } catch (error) {
+        res.send({
+            status: "error",
+            message: error,
+            data: null
+        });
+    }
+
 }
 
 exports.addMetric = async (req, res) => {
@@ -35,14 +84,12 @@ exports.addMetric = async (req, res) => {
     const metric = Metric.create({
         name: metricData.name,
         value: metricData.value
-    }).then(function (q) {
-         res.send({
+    }).then(function (metric) {
+        res.send({
              status: "success",
              message: "metric created",
-             data: {
-                 metricData,
-             }
-         });
+             data: metric
+        });
      }).catch(e => {
         res.send({
             status: "error",
